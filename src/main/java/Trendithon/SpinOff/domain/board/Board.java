b667ff53fb90ed71840682fb336f3335883ce944
minseokKim6823 <alstjr6823@naver.com>
2024-02-12 18:56:19 +0900

    first commit

--- src/main/java/Trendithon/SpinOff/domain/board/Board.java
+++ src/main/java/Trendithon/SpinOff/domain/board/Board.java
@@ -1,5 +1,6 @@
 package Trendithon.SpinOff.domain.board;
 
+import Trendithon.SpinOff.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +16,7 @@ public class Board {
 
     @ManyToOne
     @JoinColumn(name = "user_id") // 매핑할 외부 엔티티의 컬럼명 지정
-//  private Member user;
+    private Member user;
 
     private String board_title;
     private String board_comment;
